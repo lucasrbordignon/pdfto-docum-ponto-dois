0c567b2610d373b2a0820a70e9836c7ae2c81b8a
Programador04 <Programador04@12info.corp>
2023-02-14 11:02:48 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,62 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { Buffer } from 'buffer';
+import { PdfApi } from 'asposepdfcloud';
+import * as fs from 'fs';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @Post('/pdf-to-doc')
+  async pdfToDoc(@Body() body: any) {
+
+    const { name, resultName } = body
+    const SrcFile = `/${name}`;
+    const resultPath = `MyFolder/ ${resultName}`;
+    const storageName = 'AppStorage';
+    const fileToWrite = process.cwd() + '/' + resultName;
+    const pdfApi = new PdfApi(
+      '31142b24-ad39-48c9-99c3-ebe1f061a8da',
+      '2972ac0203e5a649ddaf7f786e332bf3',
+    );
+    try {
+      const data = fs.readFileSync(name);
+      await pdfApi.uploadFile(SrcFile, Buffer.from(data), storageName);
+      await pdfApi.putPdfInStorageToDoc(
+        name,
+        resultPath,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        storageName,
+      );
+
+      // Baixar pdf do armazenamento em nuvem
+      const fileData = await pdfApi.downloadFile(resultPath, storageName, '');
+      const writeStream = fs.createWriteStream(fileToWrite);
+      writeStream.write(fileData.body);
+      return Buffer.from(data).toString('base64')
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Post('/addPDF')
+  async addPDf(@Body() body: any): Promise<string> {
+    const { name, stringToDecode } = body
+
+    fs.writeFile(name, stringToDecode, 'base64', (error) => {
+      if (error) throw error.message;
+      console.log("PDF salvo!");
+    });
+    return 'Sucesso'
+  }
+}
